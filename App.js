c16eea0b38bf38e4fbac2b7ba4289a6b66f0e9ab
Aluno <terraarthur95@gmail.com>
2024-12-03 21:28:43 -0300

    mais um mini progresso

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Alexandria</Text>
-      <StatusBar style="auto" />
+      <TextInput>
+        
+      </TextInput>
     </View>
   );
 }
@@ -13,13 +16,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#D3D3D3',
+    backgroundColor: '#fff',
     justifyContent: 'flex-start',
     alignItems: 'center',
     paddingTop: 20
   },
   text: {
     fontSize: 30,
-    fontStyle: 'italic'
+    fontStyle: 'italic',
+    marginBottom: 2,
+  },
+  livros:{
+    borderWidth: 1,
+    borderColor: '#ccc',
   }
 });
